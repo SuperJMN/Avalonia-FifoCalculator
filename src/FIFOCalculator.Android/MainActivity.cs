f0cb85adee65535f9f402b2629aee18cdde917cf
José Manuel Nieto <superjmn@outlook.com>
2023-08-08 10:27:00 +0200

    Upgrade to Avalonia 11

--- src/FIFOCalculator.Android/MainActivity.cs
+++ src/FIFOCalculator.Android/MainActivity.cs
@@ -1,12 +1,24 @@
 ﻿using Android.App;
 using Android.Content.PM;
+using Avalonia;
 using Avalonia.Android;
+using Avalonia.ReactiveUI;
 
 namespace FIFOCalculator.Android
 {
-    [Activity(Label = "SeaweedFS.Gui.Android", Theme = "@style/MyTheme.NoActionBar", Icon = "@drawable/icon",
-        LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
-    public class MainActivity : AvaloniaMainActivity
+    [Activity(
+        Label = "FIFO Calculator",
+        Theme = "@style/MyTheme.NoActionBar",
+        Icon = "@drawable/icon",
+        MainLauncher = true,
+        ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
+    public class MainActivity : AvaloniaMainActivity<App>
     {
+        protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
+        {
+            return base.CustomizeAppBuilder(builder)
+                .WithInterFont()
+                .UseReactiveUI();
+        }
     }
 }
